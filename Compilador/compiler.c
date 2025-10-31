f8c3702e373c87b498f2b7630379241ca1c6f351
ClubeDoHardware <hardwareufc@gmail.com>
2025-10-31 15:03:31 -0300

    Compilador

--- Compilador/compiler.c
+++ Compilador/compiler.c
@@ -27,14 +27,14 @@ OpcodeInfo opcodes[] = { // TODO ia implementar uma tabela pra ficar mais facil 
     {"HALT", "00000001", 0, 'N','N'},
     // Memoria/mov
     {"MOV", "00010000", 2, 'R','R'},
-    {"MOV", "00010001", 2, 'V','R'},
+    {"MOV", "00010001", 2, 'R','V'},
     {"LOAD", "00010010", 2, 'M','R'},
     {"STORE", "00010011", 2, 'R','M'},
     // Aritmetica
     {"ADD", "01000000", 2, 'R','R'},
-    {"ADD", "01010000", 2, 'V','R'},
+    {"ADD", "01010000", 2, 'R','V'},
     {"SUB", "01000001", 2, 'R','R'},
-    {"SUB", "01010001", 2, 'V','R'},
+    {"SUB", "01010001", 2, 'R','V'},
     {"MUL", "01000010", 2, 'R','R'},
     {"DIV", "01000011", 2, 'R','R'},
     {"MOD", "01000100", 2, 'R','R'},
@@ -42,13 +42,20 @@ OpcodeInfo opcodes[] = { // TODO ia implementar uma tabela pra ficar mais facil 
     {"DEC", "01000110", 1, 'R','N'},
     // Logica
     {"AND", "01100000", 2, 'R','R'},
-    {"AND", "01101000", 2, 'V','R'},
+    {"AND", "01101000", 2, 'R','V'},
     {"OR", "01100001", 2, 'R','R'},
+    {"OR", "01101001", 2, 'R','V'},
     {"NOT", "01100010", 1, 'R','N'},
     {"XOR", "01100011", 2, 'R','R'},
+    {"XOR", "01101010", 2, 'R','V'},
     {"NAND", "01100100", 2, 'R','R'},
     {"NOR", "01100101", 2, 'R','R'},
     {"XNOR", "01100110", 2, 'R','R'},
+    // Shift/Bit
+    {"SHL", "01110000", 1, 'R','N'},
+    {"SHR", "01110001", 1, 'R','N'},
+    {"CMP", "01111000", 2, 'R','R'},
+    {"CMP", "01111001", 2, 'R','V'},
     {"End", "", 0} // So pra saber quando essa porra terminou
 };
 
@@ -118,7 +125,6 @@ int compileInstruction(char* mnemonic, char* parameters[], int nParameters){ // 
             parametersTypes[i] = 'N';
         }
     }
-    
     // -------------------------------------------------------
     
     if(parametersTypes == NULL){return 1;} // Em caso de erro
@@ -159,11 +165,23 @@ int compileInstruction(char* mnemonic, char* parameters[], int nParameters){ // 
             i=-2;
         }
         else if(strcmp(opcodes[i].mnemonic, "End") == 0){
-            return 0;
+            return 1;
             i=-2;
         }
         i++;
     }
+    // Adicionar linhas adicionais para manter o numero fixo de 3 bytes de instrucao
+    if(nParameters==0){
+        fwrite("00000000", sizeof(char), 8, binaryFile);
+        fwrite("\n", sizeof(char), 1, binaryFile); // Faz a quebra de linha
+        fwrite("00000000", sizeof(char), 8, binaryFile);
+        fwrite("\n", sizeof(char), 1, binaryFile); // Faz a quebra de linha
+    }
+    else if(nParameters==1){
+        fwrite("00000000", sizeof(char), 8, binaryFile);
+        fwrite("\n", sizeof(char), 1, binaryFile); // Faz a quebra de linha
+    }
+    return 0;
 }
 int compileLine(char line[], int lineNumber){
     // Remover comentarios
@@ -213,14 +231,14 @@ int compileLine(char line[], int lineNumber){
 
 int main(){
     // Abrir o arquivo assembly que vai ser lido
-    FILE* assemblyFile = fopen("output/assembly.txt", "r");
+    FILE* assemblyFile = fopen("assembly.txt", "r");
     if(assemblyFile == NULL){
         printf("%s", "Erro ao abrir o arquivo assembly!");
         return 1;
     }
 
     // Criar novo arquivo compilado em binario
-    binaryFile = fopen("binary.bin", "wb");
+    binaryFile = fopen("binary.txt", "wb");
     if(binaryFile == NULL){
         printf("Erro ao gerar codigo binario!");
         return 1;
@@ -230,8 +248,10 @@ int main(){
     
     int lineNumber = 1; // Numero para contar em qual linha o leitor se encontra
     while(fgets(buffer, 1024, assemblyFile) != NULL){
+        if(buffer[0]!='\n'){
+            compileLine(removeSpaces(buffer), lineNumber); // Retorna 1 caso a linha seja compilada com sucesso e 0 se tiver algum erro.
+        }
         //Remove espacos indesejados da linha
-        compileLine(removeSpaces(buffer), lineNumber); // Retorna 1 caso a linha seja compilada com sucesso e 0 se tiver algum erro.
         lineNumber = lineNumber + 1;
     }
     if(error){
